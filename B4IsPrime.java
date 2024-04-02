dceda86fe13811960390d033e2984886bb297460
pat-rick-liu <patrick.liu25@ycdsbk12.ca>
2024-04-02 17:00:51 -0400

    5 dome

--- B4IsPrime.java
+++ B4IsPrime.java
@@ -7,12 +7,18 @@ class B4IsPrime extends ConsoleProgram {
   
   public void run() {
     int intInput;
+    int intN = 0;
     intInput = readInt("Input int: ");
 
     for(int intDivisor = 1; intDivisor <= intInput; intDivisor++){
         if (intInput % intDivisor == 0) {
-          System.out.println(intDivisor);
+          intN += 1;
         }
+    }
+    if (intN == 2) {
+      System.out.println("The integer is prime");
+    }else{
+      System.out.println("The integer is composite");
     }
   }
 }
